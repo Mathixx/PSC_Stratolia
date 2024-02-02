8d67c1d6d7b4ebd2f10a9b764f432be3f3d700cf
h1ppox99 <hippolyte.wallaert@polytechnique.edu>
2024-02-02 17:44:23 +0100

    Ecriture test2

--- Algo Naif/test_makeTree.py
+++ Algo Naif/test_makeTree.py
@@ -53,7 +53,7 @@ def choisir_ville_au_hasard(liste_villes):
     return random.choice(liste_villes)
 
 
-def test_makeTree():
+def test1_makeTree():
     depart = choisir_ville_au_hasard(villes_france)
     print("Départ : "+ str(depart))
     dest = choisir_ville_au_hasard(villes_france)
@@ -66,7 +66,8 @@ def test_makeTree():
     temps_sec = 0     # À modifier si besoin
     pression = random.randint(0,16)
     print("Pression de départ : "+str(pression))
-    duree = int(input("Veuillez entrer la durée d'exploration (nombre d'heures divisible par 6) : "))
+    # duree = int(input("Veuillez entrer la durée d'exploration (nombre d'heures divisible par 6) : "))
+    duree = 18
     
     # Paramètres de test
     destination = (dest.long, dest.lat)
@@ -81,7 +82,30 @@ def test_makeTree():
     temps_fin_execution = time.time()
     duree_execution = temps_fin_execution - temps_debut_execution
     print(f"Le code a pris {duree_execution} secondes pour s'exécuter.")
+    
+    # Renvoie si on a atteint la destination et le temps d'exécution.
+    return (a_atteint_destination, duree_execution)
 
 
 # Exécution du test
-test_makeTree()
+#test1_makeTree()
+
+
+def test2_makeTree():
+    # On fait varier la limite d'éloignement et la durée dans test1
+    nombre_tests = 100
+    moyenne_temps = 0
+    moyenne_chemins_trouves = 0
+    for i in range(nombre_tests):
+        (a_atteint_destination, duree_execution) = test1_makeTree()
+        if a_atteint_destination:
+            moyenne_chemins_trouves += 1
+        moyenne_temps += 1
+    moyenne_chemins_trouves /= nombre_tests
+    moyenne_temps /= nombre_tests
+    print ("La fréquence de chemins trouvés est de : " + str(moyenne_chemins_trouves) +" %.")
+    print ("La moyenne temporelle est de : " + str(moyenne_temps) +" secondes.")
+
+
+# Exécution du test
+#test2_makeTree()
